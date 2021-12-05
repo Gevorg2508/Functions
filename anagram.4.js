960f36ba16fa5fc58eabcfab3cc88782eda0aad3
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-05 15:51:50 +0400

    feat: anagram.4.js

---
+++ anagram.4.js
@@ -0,0 +1,11 @@
+ let mainArr = ['licnep', 'circular', 'pupil', 'nilcpe', 'leppnec'];
+let result = [] ; 
+ function anagram(word,...arr){
+    let word1 = word.split('').sort().join('');
+    for(let i = 0 ; i <arr.length ; i ++){
+       if (arr[i].split('').sort().join('').includes(word1)){
+         console.log(arr[i].split( ));
+        }
+    }
+}
+ anagram("pencil",...mainArr);
