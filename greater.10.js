2c6b8511338721e7346e26b7fa813bfe172aaf42
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-10 02:17:50 +0400

    fix: remove greater.10.js

--- greater.10.js
+++
@@ -1,4 +0,0 @@
-let func = function(arr,number){
-    return arr.filter(elem => elem > number).length === 0 ? "Such values do not exists" :arr.filter(elem => elem > number)
-;}
-console.log(func([10, 25, 16, -5, 30, 15, 24],16));
